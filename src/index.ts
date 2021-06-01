69af1b2e1a5ce06abd1b82688a39097419901b6d
rafa <rafaelsistem87@gmail.com>
2021-06-01 12:01:35 -0700

    atomic design

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export * as ButtonComponent from "./atoms/ButtonComponent"
+export * as TestComponent from "./atoms/TestComponent";
+export * as TextInput  from './atoms/TextInput/TextInput';
 
 
-import TestComponent from "./TestComponent";
-import ButtonComponent from "./ButtonComponent"
 
-export { TestComponent, ButtonComponent };
