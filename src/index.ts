d7119851feeefb71900f05d3df1d36c009038056
rafa <rafaelsistem87@gmail.com>
2021-06-02 09:25:34 -0700

    17

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-
-
+import TextInput from './TextInput';
 import TestComponent from "./TestComponent";
 import ButtonComponent from "./ButtonComponent"
 
-export { TestComponent, ButtonComponent };
+export { TestComponent, ButtonComponent, TextInput };
+ 
