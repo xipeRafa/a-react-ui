d26d5077a0a97b8beae57e7a53c3ef5e7269004f
rafa <rafaelsistem87@gmail.com>
2021-06-02 09:19:27 -0700

    last1

---
+++ src/ButtonComponent/ButtonComponent.types.ts
@@ -0,0 +1,10 @@
+// Generated with util/create-component.js
+
+export interface ButtonComponentProps {
+    primary?: boolean;
+    backgroundColor?: string;
+    size?: 'small' | 'medium' | 'large';
+    label: string;
+    onClick?: () => void;
+  }
+  
